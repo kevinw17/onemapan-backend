5e0398fc255434039073648608a1000d2380c498
Kevin Winata <kevin.winata170600@gmail.com>
2025-11-07 13:42:46 +0700

    fix fetching list of dianchuanshi

--- src/dianchuanshi/dianchuanshi.repository.ts
+++ src/dianchuanshi/dianchuanshi.repository.ts
@@ -2,6 +2,13 @@ import prisma from "../db";
 
 export const findAllDianChuanShi = async () => {
     return await prisma.dianChuanShi.findMany({
+        select: {
+        id: true,
+        name: true,
+        mandarin_name: true,
+        area: true,
+        is_fuwuyuan: true,
+        },
         orderBy: { name: "asc" },
     });
 };
